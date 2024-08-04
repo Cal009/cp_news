5e19d746129eb4ffcbce50547a71e0631c987877
Callum Priestman <callum.priestman@hotmail.co.uk>
2024-08-04 14:40:10 +0000

    like button functioning with like counter

--- news/views.py
+++ news/views.py
@@ -62,6 +62,7 @@ def post_detail(request, slug):
             "comments": comments,
             "comment_count": comment_count,
             "comment_form": comment_form,
+            "total_likes": total_likes,
         }
     )
 
