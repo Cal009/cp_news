b2e9ec3458d0dc0d60562bc741e6103df0672ee0
Callum Priestman <callum.priestman@hotmail.co.uk>
2024-08-13 22:11:55 +0000

    about in line with pep8

--- about/views.py
+++ about/views.py
@@ -7,10 +7,12 @@ from .forms import CollaborateForm
 def about_me(request):
 
     if request.method == "POST":
-            collaborate_form = CollaborateForm(data=request.POST)
-            if collaborate_form.is_valid():
-                collaborate_form.save()
-                messages.add_message(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS, "Collaboration request received!\
+                      I endeavour to respond within 2 working days.")
 
     """
     Renders the About page
